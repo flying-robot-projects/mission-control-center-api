f1a03ec961643f0832581a0fffe9a19029c9b7fe
Arnaud Lenglet <arlenglet@gmail.com>
2015-08-06 23:43:16 +0200

    feat(controllers): create ShipModelsController + pending tests

---
+++ app/controllers/ship_models_controller.rb
@@ -0,0 +1,15 @@
+class ShipModelsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
---
+++ spec/controllers/ship_models_controller_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "ShipModelsController", :type => :request do
+
+  pending "Index action"
+
+  pending "Create action"
+
+  pending "Update action"
+
+  pending "Destroy action"
+
+end
