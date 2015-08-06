10f9d6083c56fed1ef51a8e171426de90d8c4e00
Arnaud Lenglet <arlenglet@gmail.com>
2015-08-06 23:45:28 +0200

    feat(routes): update routes for new controllers and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :flights, only: [:index, :show], defaults: { format: :json }
+  resources :flights, only: [:index, :create, :show, :update], defaults: { format: :json }
+  resources :ships, only: [:index, :create, :show, :update, :destroy], defaults: { format: :json }
+  resources :ship_models, only: [:index, :create, :update, :destroy], defaults: { format: :json }
 end
